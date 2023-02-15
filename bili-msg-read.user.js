4efa42e984b05059971629df1bd8adc3c666c66d
kutwss <31578556+monSteRhhe@users.noreply.github.com>
2023-02-15 13:45:49 +0800

    update
    改用axios和cash

--- bili-msg-read.user.js
+++ bili-msg-read.user.js
@@ -1,41 +1,43 @@
 // ==UserScript==
 // @name         bilibili消息一键已读
 // @namespace    http://tampermonkey.net/
-// @version      1.0.2
+// @version      23.02.15
 // @description  一键设置消息已读。
 // @author       monSteRhhe
 // @match        http*://message.bilibili.com/*
 // @icon         https://www.bilibili.com/favicon.ico
-// @require      https://code.jquery.com/jquery-3.6.0.min.js
-// @run-at       document-end
 // @grant        GM_addStyle
 // @grant        GM_notification
+// @require      https://cdnjs.cloudflare.com/ajax/libs/cash/8.1.3/cash.min.js
+// @require      https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js
+// @run-at       document-end
 // ==/UserScript==
+/* globals axios, $, waitForKeyElements */
 
 (function() {
     'use strict';
 
     // 添加按钮
     GM_addStyle('\
-    .msgread {\
-        color: #6b757b;\
-        position: relative;\
-        line-height: 2rem;\
-        cursor: pointer;\
-    }\
-    .msgread:hover {\
-        color: #00a0d8;\
-    }\
+        .msgread {\
+            color: #6b757b;\
+            position: relative;\
+            line-height: 2rem;\
+            cursor: pointer;\
+        }\
+        .msgread:hover {\
+            color: #00a0d8;\
+        }\
     ')
 
-    setInterval(function() {
+    document.getElementsByClassName('container')[0].addEventListener('DOMNodeInserted', function() {
         if(document.getElementsByClassName('msgread')[0] == null) {
             $('ul.list').append('<div class="msgread">√　一键已读</div>');
         }
-    }, 1000)
+    })
 
     // 监控点击
-    $(document).on('click', '.msgread', function() {
+    $(document).on('click', 'div.msgread', function() {
         if(UnreadNum() > 0) {
             MsgSessions('')
         } else {
@@ -53,16 +55,12 @@
         var api = 'https://api.vc.bilibili.com/session_svr/v1/session_svr/single_unread';
         var num = 0;
 
-        $.ajax({
+        axios({
             url: api,
-            type: 'GET',
-            async: false,
-            xhrFields: {
-                withCredentials: true
-            },
-            success: function(result) {
-                num = result.data.unfollow_unread + result.data.follow_unread;
-            }
+            withCredentials: true // 跨域使用凭证
+        })
+        .then(function(response) {
+            num = response.data.data.unfollow_unread + response.data.data.follow_unread;
         })
 
         return num;
@@ -77,36 +75,32 @@
             api = 'https://api.vc.bilibili.com/session_svr/v1/session_svr/get_sessions?session_type=1&group_fold=1&unfollow_fold=0&sort_rule=2&end_ts=' + endts + '&build=0&mobi_app=web';
         }
 
-        $.ajax({
+        axios({
             url: api,
-            type: 'GET',
-            async: false,
-            xhrFields: {
-                withCredentials: true
-            },
-            success: function(result) {
-                var session_list = result.data.session_list;
-                for(var i = 0; i < session_list.length; i++) {
-                    if(session_list[i]['unread_count'] > 0) {
-                        var ti = session_list[i]['talker_id'];
-                        var as = session_list[i]['ack_seqno'];
-                        var bj = GetCookie();
+            withCredentials: true // 跨域使用凭证
+        })
+        .then(function(response) {
+            var session_list = response.data.data.session_list;
+            for(var i = 0; i < session_list.length; i++) {
+                if(session_list[i].unread_count > 0) {
+                    var ti = session_list[i].talker_id;
+                    var as = session_list[i].ack_seqno;
+                    var bj = GetCookie();
                         MsgRead(ti, as, bj);
                     }
-                    if(UnreadNum() > 0) {
-                        if(i == session_list.length - 1) {
-                            var endts = session_list[i]['session_ts'];
-                            MsgSessions(endts);
-                        }
-                    } else {
-                        GM_notification({
-                            text: '消息全部已读。',
-                            title: 'Bili.Msg.Read',
-                            image: 'https://www.bilibili.com/favicon.ico',
-                            timeout: 2000,
-                        });
-                        break;
+                if(UnreadNum() > 0) {
+                    if(i == session_list.length - 1) {
+                        var endts = session_list[i].session_ts;
+                        MsgSessions(endts);
                     }
+                } else {
+                    GM_notification({
+                        text: '消息全部已读。',
+                        title: 'Bili.Msg.Read',
+                        image: 'https://www.bilibili.com/favicon.ico',
+                        timeout: 2000,
+                    });
+                    break;
                 }
             }
         })
@@ -116,13 +110,10 @@
     function MsgRead(ti, as, bj) {
         var api = 'https://api.vc.bilibili.com/session_svr/v1/session_svr/update_ack?talker_id=' + ti + '&session_type=1&ack_seqno=' + as + '&build=0&mobi_app=web&csrf_token=' + bj +'&csrf=' + bj;
 
-        $.ajax({
+        axios({
+            method: 'post',
             url: api,
-            type: 'POST',
-            async: true,
-            xhrFields: {
-                withCredentials: true
-            }
+            withCredentials: true // 跨域使用凭证
         })
     }
 
